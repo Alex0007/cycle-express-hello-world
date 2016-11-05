82fec7578d544c8ed642bf42c9b7922ad94975b1
Alex0007 <Alex0007lolpvp@gmail.com>
2016-11-05 12:16:50 +0500

    updates

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
+import Rx from 'rxjs/Rx'
 import { run } from '@cycle/rxjs-run'
-import { makeRouterDriver } from 'cycle-express'
+import makeRouterDriver from 'cycle-express'
 import express from 'express'
+
+import NestedComponent from './nested'
 
 let app = express()
 let router = express.Router()
 
-function main ({ router }) {
+function main({ router }) {
+  const nested = NestedComponent({ router })
+
+  const mainHandler$ = router.get('/').map(({ id }) => {
+    return { id, send: 'This is an example of cycle.js driver for express application' }
+  })
+
+  const numberHandler$ = router.get('/number/:number')
+    .map(req => {
+      return {
+        id: req.id,
+        send: `Hello world, ${req.params.number}`
+      }
+    })
+
   return {
-    router: router.get('*')
-      .map(req => {
-        return {
-          id: req.id,
-          send: 'Hello world'
-        }
-      })
+    router: Rx.Observable.merge(mainHandler$, numberHandler$, nested.router)
   }
 }
 
---
+++ nested.js
@@ -0,0 +1,17 @@
+import Rx from 'rxjs/Rx'
+
+const NestedComponent = ({ router }) => {
+  const nestedRouter$ = router.route('/nested')
+
+  const nestedHandler$ = nestedRouter$
+    .get('/123')
+    .map(({ id }) => { return { id, send: 'nested router' } })
+
+
+  return {
+    router: Rx.Observable
+      .merge(nestedHandler$)
+  }
+}
+
+export default NestedComponent
