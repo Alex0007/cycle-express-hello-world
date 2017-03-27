fc3ffb9dd42353a48d9ea2d4772e9213da91acb4
Alex0007 <Alex0007lolpvp@gmail.com>
2017-03-27 15:09:05 +0500

    updates

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
-import Rx from 'rxjs/Rx'
-import { run } from '@cycle/rxjs-run'
-import { makeRouterDriver } from 'cycle-express'
-import express from 'express'
+const { Observable } = require('rxjs')
+const { makeRouterDriver } = require('cycle-express-driver')
+const express = require('express')
+const run = require('@cycle/rxjs-run').default
 
-import NestedComponent from './nested'
-
-let app = express()
-let router = express.Router()
+const app = express()
+const router = express.Router()
 
 function main({ router }) {
-  const nested = NestedComponent({ router })
-
   const mainHandler$ = router.get('/').map(({ id }) => {
     return { id, send: 'This is an example of cycle.js driver for express application' }
   })
 
-  const delayed$ = router.get('/delayed').flatMap(({id}) => {
+  const delayed$ = router.get('/123').flatMap(({ id }) => {
     return new Promise((resolve) => {
-      setTimeout(() => resolve({id, send: 'this was delayed'}), 3000)
+      setTimeout(() => { resolve({ id, send: 'ok' }) }, 1000)
     })
   })
 
-  const numberHandler$ = router.get('/number/:number')
-    .map(req => {
-      return {
-        id: req.id,
-        send: `Hello world, ${req.params.number}`
-      }
-    })
-
   return {
-    router: Rx.Observable.merge(mainHandler$, numberHandler$, delayed$, nested.router)
+    router: Observable.merge(
+      mainHandler$, delayed$
+    )
   }
 }
 
@@ -38,4 +28,6 @@ run(main, {
   router: makeRouterDriver(router)
 })
 
-app.use(router).listen(3000)
+app.use(router).listen(3000, () => {
+  console.log('Listening on http://localhost:3000/')
+})
